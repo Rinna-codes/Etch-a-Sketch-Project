fab2171a259e9f33fca1a7c6781c14734e45a12b
Zharinna Subido <zbsxph@mail.umkc.edu>
2026-03-21 12:49:52 -0500

    created a button and prompt for the user to click and enter their preferred grid size.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const container = document.getElementById("grid-container");
+const gridSizeBtn = document.getElementById('grid-number');
 
-function createGridCells() {
-    for (let i = 0; i < 256; i++) { // 256 came from the total of 16x16 
+function createGridCells(userInput) {
+    const gridSize = userInput * userInput;
+    for (let i = 0; i < gridSize; i++) { // 256 came from the total of 16x16 
         const cells = document.createElement('div');
         cells.classList.add('grid-cell'); // made a class called 'grid-cell' to appear on html web page
         container.appendChild(cells); // what made the cells to appear in the web page
@@ -15,4 +17,16 @@ container.addEventListener('mouseover', (event) => {
     }
 });
 
-createGridCells()
+gridSizeBtn.addEventListener('click', () => {
+    const userInput = prompt("What grid size would you like to set (#x#): ");
+
+    if (userInput >= 100) {
+        alert("Woah, that's a bit too big! Please re-enter a bit smaller grid size");
+        newInput = prompt("What grid size would you like to set (#x#): ");
+        createGridCells(newInput);
+    } else {
+        createGridCells(userInput);
+    }
+});
+
+baseGrid();
